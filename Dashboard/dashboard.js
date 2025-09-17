941d216866361b34902ac45733905254d4bb8e82
Abdulla Al Masud <112339417+masudabdullaa@users.noreply.github.com>
2025-09-17 08:09:36 +0600

    Add files via upload

---
+++ Dashboard/dashboard.js
@@ -0,0 +1,26 @@
+function square(num1, num2) {
+    let results = num1 * num2;
+    console.log("Multiply is : " + results);
+}
+
+function addetion(add1, add2){
+    let add = add1 + add2;
+    console.log("Addetion is : " + add);
+    
+}
+function subtruction(sub1, sub2){
+    let sub = sub1 - sub2;
+    console.log("Subtruction is : " + sub);
+}
+
+square(5, 8);
+square(8, 8);
+square(9, 8);
+addetion(9, 9);
+addetion(90, 19);
+addetion(97, 9);
+addetion(96, 9);
+subtruction(30, 2);
+subtruction(30, 12);
+subtruction(30, 20);
+subtruction(30, 22);
